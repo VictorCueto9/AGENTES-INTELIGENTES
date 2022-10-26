67189019a066f0ab53038e39ff6293e335cdcce1
VictorCueto9 <cueto874@gmail.com>
2022-10-26 18:24:54 -0500

    practica 5

---
+++ Perceptron_Iris_Plant/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Perceptron_Iris_Plant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbplants = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btperceptron = new System.Windows.Forms.Button();
+            this.lbdatos = new System.Windows.Forms.ListBox();
+            this.lbresultados = new System.Windows.Forms.ListBox();
+            this.dgverdad = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgverdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbplants
+            // 
+            this.cbplants.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbplants.FormattingEnabled = true;
+            this.cbplants.Items.AddRange(new object[] {
+            "Setosa-Versicolor",
+            "Setosa-Virginica",
+            "Versicolor-Virginica"});
+            this.cbplants.Location = new System.Drawing.Point(13, 58);
+            this.cbplants.Name = "cbplants";
+            this.cbplants.Size = new System.Drawing.Size(143, 23);
+            this.cbplants.TabIndex = 0;
+            this.cbplants.SelectedIndexChanged += new System.EventHandler(this.cbplants_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Elegir clases";
+            // 
+            // btperceptron
+            // 
+            this.btperceptron.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btperceptron.Location = new System.Drawing.Point(16, 107);
+            this.btperceptron.Name = "btperceptron";
+            this.btperceptron.Size = new System.Drawing.Size(113, 65);
+            this.btperceptron.TabIndex = 2;
+            this.btperceptron.Text = "Perceptrón";
+            this.btperceptron.UseVisualStyleBackColor = true;
+            this.btperceptron.Click += new System.EventHandler(this.btperceptron_Click);
+            // 
+            // lbdatos
+            // 
+            this.lbdatos.FormattingEnabled = true;
+            this.lbdatos.Items.AddRange(new object[] {
+            " "});
+            this.lbdatos.Location = new System.Drawing.Point(162, 12);
+            this.lbdatos.Name = "lbdatos";
+            this.lbdatos.Size = new System.Drawing.Size(173, 342);
+            this.lbdatos.TabIndex = 3;
+            // 
+            // lbresultados
+            // 
+            this.lbresultados.FormattingEnabled = true;
+            this.lbresultados.Items.AddRange(new object[] {
+            " "});
+            this.lbresultados.Location = new System.Drawing.Point(341, 11);
+            this.lbresultados.Name = "lbresultados";
+            this.lbresultados.Size = new System.Drawing.Size(160, 342);
+            this.lbresultados.TabIndex = 4;
+            // 
+            // dgverdad
+            // 
+            this.dgverdad.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgverdad.Location = new System.Drawing.Point(513, 12);
+            this.dgverdad.Name = "dgverdad";
+            this.dgverdad.Size = new System.Drawing.Size(501, 341);
+            this.dgverdad.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 367);
+            this.Controls.Add(this.dgverdad);
+            this.Controls.Add(this.lbresultados);
+            this.Controls.Add(this.lbdatos);
+            this.Controls.Add(this.btperceptron);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbplants);
+            this.Name = "Form1";
+            this.Text = "Perceptron Iris Plant";
+            ((System.ComponentModel.ISupportInitialize)(this.dgverdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbplants;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btperceptron;
+        private System.Windows.Forms.ListBox lbdatos;
+        private System.Windows.Forms.ListBox lbresultados;
+        private System.Windows.Forms.DataGridView dgverdad;
+    }
+}
+
---
+++ Perceptron_Iris_Plant/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace Perceptron_Iris_Plant
+{
+    public partial class Form1 : Form
+    {
+        string line;
+        int entrada = 5; double umbral; char _clase;
+        public Form1()
+        {
+            InitializeComponent();
+            printData();
+            
+        }
+        private void printData()
+        {
+            StreamReader sr = new StreamReader("iris.txt");
+
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                lbdatos.Items.Add(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        public void createTable()
+        {
+            string column_name;
+            //se anaden las columnas con sus respectivos nombres
+            dgverdad.Columns.Add("index", " ");
+            for (int i = 4; i > 0; i--)
+            {
+                column_name = "X".Insert(1, i.ToString());
+                dgverdad.Columns.Add("entrada".Insert(7, i.ToString()), column_name);
+            }
+            dgverdad.Columns.Add("yesp", "Yesp");
+            dgverdad.Columns.Add("ycalc", "Ycalc");
+
+            for (int i = 0; i < 99; i++)
+                dgverdad.Rows.Add();
+        }
+        public void fillCells(double[] doubles, int row)
+        {
+            int i = 1;
+
+            foreach (double values in doubles)
+            {
+                dgverdad.Rows[row].Cells[0].Value = row + 1;
+                dgverdad.Rows[row].Cells[i].Value = values;
+                i++;
+            }
+
+            dgverdad.Rows[row].Cells[5].Value = row + 1 <= 50 ? -1 : 1;
+        }
+        public double[] getDoubles(string[] cadena)
+        {
+            double[] doubles = new double[cadena.Length - 1];
+            int i = 0;
+
+            foreach (string num in cadena)
+            {
+                if (!num.Contains("I"))
+                {
+                    doubles[i] = Convert.ToDouble(num);
+                }
+                i++;
+            }
+
+            return doubles;
+        }
+        public void fillTable(int comb)
+        {
+            int aux = 0;
+            string[] division;
+
+            if (comb == 1)
+            {
+                StreamReader sr = new StreamReader("iris.txt");
+                line = sr.ReadLine();
+                while (line != null &&  aux < 100)
+                {
+                    //lbresultados.Items.Add(line);
+
+                    division = line.Split(',');
+                    fillCells(getDoubles(division), aux);
+
+                    line = sr.ReadLine();
+                    
+                    aux++;
+                }
+                sr.Close();
+            }
+            else if (comb == 2)
+            {
+                StreamReader sr = new StreamReader("iris.txt");
+                aux = 0; int row = 0;
+                line = sr.ReadLine();
+                while (line != null && row < 100 && aux < 150)
+                {
+                    if (aux < 50 || aux > 99)
+                    {
+                        //lbresultados.Items.Add(line);
+                        division = line.Split(',');
+                        fillCells(getDoubles(division), row);
+                        row++;
+                    }
+                    line = sr.ReadLine();
+                    aux++;
+                }
+                sr.Close();
+            }
+            else
+            {
+                StreamReader sr = new StreamReader("iris.txt");
+                aux = 0; int row = 0;
+                line = sr.ReadLine();
+                while (line != null && row < 100 && aux < 150)
+                {
+                    if (aux > 49)
+                    {
+                        //lbresultados.Items.Add(line);
+                        division = line.Split(',');
+                        fillCells(getDoubles(division), row);
+                        row++;
+                    }
+                    line = sr.ReadLine();
+                    aux++;
+                }
+                sr.Close();
+            }
+        }
+        private void cbplants_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = cbplants.SelectedIndex;
+
+            dgverdad.Columns.Clear();
+            lbresultados.Items.Clear();
+
+            switch (index)
+            {
+                case 0:
+                    createTable();
+                    fillTable(1);
+                    break;
+                case 1:
+                    createTable();
+                    fillTable(2);
+                    break;
+                case 2:
+                    createTable();
+                    fillTable(3);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btperceptron_Click(object sender, EventArgs e)
+        {
+            double[] peso = new double[entrada];
+            int fila = 0, epoca = 1;
+            double ycalc, aux;
+            bool esIgual = true, correcto = false;
+
+            peso = generaAleatorios();
+            imprimeList(peso, epoca, false);
+            lbresultados.Items.Add("Epoca = " + epoca.ToString());
+            lbresultados.Items.Add("");
+
+            while (fila != dgverdad.Rows.Count && epoca < 52)
+            {
+                ycalc = evalua(peso, fila);
+                lbresultados.Items.Add("Entrada: " + fila.ToString());
+                esIgual = verifica(ycalc, fila);
+                if (esIgual)
+                {
+                }
+                else
+                {
+                    peso = aprendizaje(peso, _clase, fila);
+                }
+                fila++;
+                if (fila == dgverdad.Rows.Count)
+                {
+                    if (verificaResultado(peso))
+                    {
+                        correcto = true;
+                        imprimeList(peso, epoca, false);
+                        break;
+                    }
+                    else
+                    {
+                        fila = 0;
+                        epoca += 1;
+                        imprimeList(peso, epoca, false);
+                        lbresultados.Items.Add("Epoca = " + epoca.ToString());
+                        lbresultados.Items.Add("");
+                    }
+                }
+                if (epoca > 50)
+                {
+                    MessageBox.Show("Parametros NO encontrados");
+                    correcto = false;
+                    break;
+                }
+                imprimeList(peso, epoca, false);
+            }
+
+            if (correcto)
+            {
+                for (int j = 0; j < dgverdad.Rows.Count; j++)
+                {
+                    dgverdad.Rows[j].Cells[entrada + 1].Value = dgverdad.Rows[j].Cells[entrada].Value;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < dgverdad.Rows.Count; j++)
+                {
+                    aux = evalua(peso, j);
+                    dgverdad.Rows[j].Cells[entrada + 1].Value = aux > 0 ? 1 : -1;
+                }
+            }
+
+            pintaColum(false);
+            lbresultados.Items.Add("");
+            lbresultados.Items.Add("Parametros finales:");
+            imprimeList(peso, epoca, true);
+        }
+        public void imprimeList(double[] peso, int epoca, bool impEpc)
+        {
+            if (impEpc)
+            {
+                for (int i = 0; i < entrada - 1; i++)
+                {
+                    lbresultados.Items.Add("W[" + i.ToString() + "]" + " = " + peso[i].ToString());
+                }
+                lbresultados.Items.Add("Umbral = " + umbral.ToString());
+                lbresultados.Items.Add("Epocas: " + epoca.ToString());
+            }
+            else
+            {
+                for (int i = 0; i < entrada - 1; i++)
+                {
+                    lbresultados.Items.Add("W[" + i.ToString() + "]" + " = " + peso[i].ToString());
+                }
+                lbresultados.Items.Add("Umbral = " + umbral.ToString());
+            }
+            lbresultados.Items.Add("");
+        }
+        public bool verificaResultado(double[] peso)
+        {
+            double ycalc;
+
+            for (int j = 0; j < dgverdad.Rows.Count; j++)
+            {
+                ycalc = evalua(peso, j);
+                if (verifica(ycalc, j))
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+        public double[] aprendizaje(double[] peso, char clase, int fila)
+        {
+            double delta = clase == 'A' ? -1 : 1;
+            double[] newpesos = new double[entrada];
+            double[] deltax = new double[entrada];
+
+            for (int i = 0; i < peso.Length; i++)
+            {
+                deltax[i] = delta * Convert.ToDouble(dgverdad.Rows[fila].Cells[i+1].Value);
+            }
+            for (int i = 0; i < peso.Length; i++)
+            {
+                newpesos[i] = deltax[i] + peso[i];
+            }
+            umbral += delta;
+
+            return newpesos;
+        }
+        public bool verifica(double value, int fila)
+        {
+            _clase = Convert.ToDouble(dgverdad.Rows[fila].Cells[entrada].Value) == -1 ? 'A' : 'B';
+            value = value > 0 ? 1 : -1;
+            return value == Convert.ToDouble(dgverdad.Rows[fila].Cells[entrada].Value);
+        }
+
+        public double evalua(double[] peso, int fila)
+        {
+            double ycalc = 0;
+
+            for (int i = 0; i < entrada - 1; i++)
+            {
+                ycalc += peso[i] * Convert.ToDouble(dgverdad.Rows[fila].Cells[i+1].Value);
+            }
+            ycalc += umbral;
+            return ycalc;
+        }
+        public double[] generaAleatorios()
+        {
+            double[] pesos = new double[entrada - 1];
+            Random aleat = new Random();
+            for (int i = 0; i < entrada - 1; i++)
+            {
+                pesos[i] = Math.Round(aleat.NextDouble(), 2);
+            }
+            umbral = Math.Round(aleat.NextDouble(), 2);
+            /*pesos[0] = 0.84;
+            pesos[1] = 0.54;
+            pesos[2] = 0.4;
+            pesos[3] = 0.95;
+            umbral = 0.88;*/
+            return pesos;
+        }
+        public void pintaColum(bool y)
+        {
+            for (int j = 0; j < dgverdad.Rows.Count; j++)
+            {
+                if (y)
+                    dgverdad.Rows[j].Cells[entrada].Style.BackColor = Color.Beige;
+                else
+                    dgverdad.Rows[j].Cells[entrada + 1].Style.BackColor = Color.LightSkyBlue;
+            }
+        }
+
+    }
+}
