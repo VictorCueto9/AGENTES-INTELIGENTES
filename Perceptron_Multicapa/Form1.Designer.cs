7f89c61df49b88c760fd689c1a63f71d20d0eab3
VictorCueto9 <cueto874@gmail.com>
2022-11-09 10:13:02 -0600

    practica6

---
+++ Perceptron_Multicapa/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Perceptron_Multicapa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbxor = new System.Windows.Forms.RadioButton();
+            this.rbejer = new System.Windows.Forms.RadioButton();
+            this.dgverdad = new System.Windows.Forms.DataGridView();
+            this.lbres = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgverdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbxor
+            // 
+            this.rbxor.AutoSize = true;
+            this.rbxor.Checked = true;
+            this.rbxor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbxor.Location = new System.Drawing.Point(12, 25);
+            this.rbxor.Name = "rbxor";
+            this.rbxor.Size = new System.Drawing.Size(54, 23);
+            this.rbxor.TabIndex = 0;
+            this.rbxor.TabStop = true;
+            this.rbxor.Text = "XOR";
+            this.rbxor.UseVisualStyleBackColor = true;
+            // 
+            // rbejer
+            // 
+            this.rbejer.AutoSize = true;
+            this.rbejer.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbejer.Location = new System.Drawing.Point(12, 59);
+            this.rbejer.Name = "rbejer";
+            this.rbejer.Size = new System.Drawing.Size(108, 23);
+            this.rbejer.TabIndex = 1;
+            this.rbejer.Text = "Ejercicio";
+            this.rbejer.UseVisualStyleBackColor = true;
+            // 
+            // dgverdad
+            // 
+            this.dgverdad.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgverdad.Location = new System.Drawing.Point(12, 135);
+            this.dgverdad.Name = "dgverdad";
+            this.dgverdad.Size = new System.Drawing.Size(305, 154);
+            this.dgverdad.TabIndex = 2;
+            // 
+            // lbres
+            // 
+            this.lbres.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbres.FormattingEnabled = true;
+            this.lbres.ItemHeight = 18;
+            this.lbres.Items.AddRange(new object[] {
+            " "});
+            this.lbres.Location = new System.Drawing.Point(323, 38);
+            this.lbres.Name = "lbres";
+            this.lbres.Size = new System.Drawing.Size(165, 256);
+            this.lbres.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(126, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "FeedForward";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Resultados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbres);
+            this.Controls.Add(this.dgverdad);
+            this.Controls.Add(this.rbejer);
+            this.Controls.Add(this.rbxor);
+            this.Name = "Form1";
+            this.Text = "FeedForward";
+            ((System.ComponentModel.ISupportInitialize)(this.dgverdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbxor;
+        private System.Windows.Forms.RadioButton rbejer;
+        private System.Windows.Forms.DataGridView dgverdad;
+        private System.Windows.Forms.ListBox lbres;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Perceptron_Multicapa/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perceptron_Multicapa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fila = 1;
+            int entradas = 2, salidas = 1;
+            createTable(entradas, salidas);
+
+            double[] valEntradas = new double[entradas];
+            //double[] valEntradas = new double[] {0,0};
+            for (int i = 0; i < entradas; i++)
+                valEntradas[i] = Convert.ToDouble(dgverdad.Rows[0].Cells[i].Value);
+
+            //crear la Red
+
+            Red red = new Red() { capa = 3 };
+
+            int[] numNeuronas = new int[] { 2, 1 };
+            List<double[]> listUmbral = new List<double[]> { new double[] { -1.90289, -4.127002 }, new double[] { -2.570539 } };
+            List<double[,]> listPesos = new List<double[,]> { new double[,] { { 5.191129, 2.758669 }, { 5.473012, 2.769596 } }, new double[,] { { 5.839709 }, { -6.186834 } } };
+
+            red.createUmbral(numNeuronas);
+            for (int i = 0; i < listUmbral.Count; i++)
+                red.fillUmbral(listUmbral[i], i);
+
+            red.insertInput(valEntradas);
+
+            red.createPesos();
+            for (int i = 0; i < listPesos.Count; i++)
+                red.fillPesos(listPesos[i], i);
+
+            //pasar las entradas a la Red y activar las neuronas
+
+            lbres.Items.Add("active:");
+
+            while (fila <= dgverdad.Rows.Count)
+            {
+                for (int i = 1; i < red.capa; i++)
+                    red.activaNeurona(i, "sigmoide");
+                for (int i = 0; i < red.listActive.Count; i++)
+                {
+                    foreach (double val in red.listActive[i])
+                    {
+                        lbres.Items.Add(val);
+                    }
+                }
+                if (fila == dgverdad.Rows.Count) break;
+                red.listActive.Clear();
+                red.deleteInput();
+                for (int i = 0; i < entradas; i++)
+                    valEntradas[i] = Convert.ToDouble(dgverdad.Rows[fila].Cells[i].Value);
+                red.insertInput(valEntradas);
+                fila++;
+            }
+
+
+        }
+        private void createTable(int entrada, int salidas)
+        {
+            dgverdad.Columns.Clear();
+            int comb_z = 0, comb_o = 0, cols, rows;
+            double combinaciones;
+            string column_name = "";
+            bool zeros = true, ones = false;
+
+            //**se anaden las columnas con sus respectivos nombres
+            for (int i = entrada; i > 0; i--)
+            {
+                column_name = "X".Insert(1, i.ToString());
+                dgverdad.Columns.Add("entrada".Insert(7, i.ToString()), column_name);
+            }
+            //dgverdad.Columns.Add("salida", "Y"); Se anaden las salidas
+            for(int i = 0; i < salidas; i++)
+            {
+                column_name = "Y".Insert(1, (i + 1).ToString());
+                dgverdad.Columns.Add("salida".Insert(6, i.ToString()), column_name);
+            }
+
+            //****llenado de tabla de verdad****
+
+            //se agregan las filas
+            combinaciones = Math.Pow(2, (double)entrada);
+            for (int i = 0; i < combinaciones - 1; i++)
+            {
+                dgverdad.Rows.Add();
+            }
+
+            //llenar con zeros y ones
+            cols = entrada; rows = (int)combinaciones;
+            double power = (double)cols;
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (zeros)
+                    {
+                        dgverdad.Rows[j].Cells[i].Value = 0;
+                        comb_z += 1;
+                    }
+                    else if (ones)
+                    {
+                        dgverdad.Rows[j].Cells[i].Value = 1;
+                        comb_o += 1;
+                    }
+
+                    if (comb_z >= Math.Pow(2, power - 1))
+                    {
+                        zeros = false;
+                        ones = true;
+                        comb_z = 0;
+                    }
+                    else if (comb_o >= Math.Pow(2, power - 1))
+                    {
+                        zeros = true;
+                        ones = false;
+                        comb_o = 0;
+                    }
+
+                }
+                power--;
+            }
+        }
+    }
+    public class Capa
+    {
+        public int neuronas { get; set; }
+        public double[] Umbral { get; set; }
+
+    }
+    public class Peso
+    {
+        public double[,] W { get; set; }
+
+    }
+    public class Red
+    {
+        public double[] entradas { get; set; }
+        public int capa { get; set; }
+        public List<Capa> listCapas { get; set; }
+        public List<Peso> listPesos { get; set; }
+        public List<double[]> listActive { get; set; }
+
+        public Red() //parametros de los valores de las entradas y cuantas capas tiene la red
+        {
+            listCapas = new List<Capa>();
+            listPesos = new List<Peso>();
+            listActive = new List<double[]>();
+        }
+        public void insertInput(double[] ints)
+        {
+            entradas = ints;
+            listCapas.Insert(0, new Capa() { neuronas = entradas.Length, Umbral = entradas }); //capa 0
+        }
+        public void deleteInput()
+        {
+            listCapas.RemoveAt(0);
+        }
+        public void createUmbral(int[] numNeuro) //a partir de capa 1, parametro = numero de neuronas
+        {
+            for (int i = 0; i < capa - 1; i++)
+            {
+                listCapas.Add(new Capa() { neuronas = numNeuro[i] });
+            }
+        }
+        public void fillUmbral(double[] valuesUmbral, int capaU) //parametro vector con los valores de cada neurona y en que capa
+        {
+            listCapas[capaU].Umbral = valuesUmbral;
+        }
+        public void createPesos()
+        {
+            for(int i = 0; i < listCapas.Count - 1; i++)
+            {
+                listPesos.Add(new Peso() { W = new double[listCapas[i].Umbral.Length, listCapas[i + 1].Umbral.Length] });
+            }
+        }
+        public void fillPesos(double w, int capa) //llena todos los pesos de un solo valor
+        {
+            for (int i = 0; i < listPesos[capa].W.GetUpperBound(0) + 1; i++) //filas
+            {
+                for (int j = 0; j < listPesos[capa].W.GetUpperBound(1) + 1; j++) //columnas
+                {
+                    listPesos[capa].W.SetValue(w, i, j);
+                }
+            }
+        }
+        public void fillPesos(double[,] w, int capa) //lena los pesos recibiendo una matriz
+        {
+            listPesos[capa].W = w;
+        }
+        public void activaNeurona(int capa, string funcion)
+        {
+            double[] active = new double[listCapas[capa].Umbral.Length];
+            int aux = 0; double sum = 0;
+
+            for(int j = 0; j < listCapas[capa].Umbral.Length; j++)
+            {
+                for (int i = 0; i < listCapas[capa - 1].Umbral.Length; i++)
+                {
+                    sum += capa == 1 ? listCapas[capa - 1].Umbral[i] * listPesos[capa - 1].W[i, j] : listActive[capa - 2][i] * listPesos[capa - 1].W[i, j];
+                }
+                sum += listCapas[capa].Umbral[aux];
+                sum = funcion == "sigmoide" ? Sigmoide(sum) : Math.Tanh(sum);
+                active[aux] = sum;
+                sum = 0; aux++;
+            }
+
+            listActive.Add(active);
+        }
+        private double Sigmoide (double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+    }
+    
+}
